78d2e632c672376b8ba9b9d4bb42d5732190441b
srreyesalanis <sr.reyes.alanis@gmail.com>
2026-05-18 05:23:10 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,11 +92,11 @@ else:
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("👤 Crear Jugador", type="primary", use_container_width="True"):
+        if st.button("👤 Crear Jugador", type="primary", use_container_width=True):
             st.session_state["page"] = "crear_jugador"
 
     with col2:
-        if st.button("🏌️ Crear Ronda", type="primary", use_container_width="True"):
+        if st.button("🏌️ Crear Ronda", type="primary", use_container_width=True):
             st.session_state["page"] = "crear_ronda"
 
     st.markdown("---")
