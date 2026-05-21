ef7b00dd26834aef3dd557272e96f052f0b03801
srreyesalanis <sr.reyes.alanis@gmail.com>
2026-05-21 06:42:50 +0000

    1

--- handicap_index.py
+++ handicap_index.py
@@ -55,7 +55,7 @@ def calcular_handicap_index(
         .update({
             "current_handicap": handicap_index
         }) \
-        .eq("golfer_id", player_id) \
+        .eq("player_id", player_id) \
         .execute()
 
     # =========================
